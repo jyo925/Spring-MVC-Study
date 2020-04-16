d25e90a4d6123ab63cc2608d91644b6f0cca3c59
jiyoon <jyo925@naver.com>
2020-04-16 11:07:56 +0900

    fisrt

---
+++ ex00/src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,47 @@
+package org.zerock.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
+public class RootConfig {
+	
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		HikariConfig hikariConfig = new HikariConfig();
+//		hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		hikariConfig.setJdbcUrl("jdbc:mysql://localhost/board");
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/board?serverTimezone=UTC");
+		hikariConfig.setUsername("root");
+		hikariConfig.setPassword("1234");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+	
+	/* mybatis의 핵심 객체  SqlSessionFactory, SQLSession*/
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory)sqlSessionFactory.getObject();
+	}
+
+
+}
